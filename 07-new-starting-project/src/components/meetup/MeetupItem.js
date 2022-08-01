5f22a9b4441279d93756fcdf816bb76522d4bff2
Matheus Lima <matheusinclima@gmail.com>
2022-08-01 20:26:27 -0300

    commit

--- 07-new-starting-project/src/components/meetup/MeetupItem.js
+++ 07-new-starting-project/src/components/meetup/MeetupItem.js
@@ -1,23 +1,45 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
+  const favoriteCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoriteCtx.itemIsFavorite(props.id);
+
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoriteCtx.removeFavorite(props.id);
+    } else {
+      favoriteCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      });
+    }
+  }
+
   return (
-    <Card>
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favorites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? "Remove from Favorites" : "To Favorites"}
+          </button>
+        </div>
+      </Card>
     </li>
-    </Card>
   );
 }
 
--- 07-new-starting-project/src/pages/NewMeetup.js
+++ 07-new-starting-project/src/pages/NewMeetup.js
@@ -1,10 +1,31 @@
 import NewMeetupForm from "../components/meetup/NewMeetupForm";
+import {useHistory} from "react-router-dom";
 
 function NewMeetupsPage() {
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm/>
+
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-app-starter-7fd20-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
+  );
 }
 
 export default NewMeetupsPage;
